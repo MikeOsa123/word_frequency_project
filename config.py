d73515dd5db6c3989ebaa24fd47b1699e8815f7f
MikeOsa123 <m.osamwonyi@gmail.com>
2020-09-04 19:33:01 +0100

    created posgres db and migrations

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-should-be-secret-duh'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
